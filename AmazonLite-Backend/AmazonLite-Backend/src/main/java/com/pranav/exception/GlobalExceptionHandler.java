a9919b6d104e95c6a3ff3efe5754a937d4b03130
Pranav Saharawat <iampranav12@gmail.com>
2024-11-20 19:54:05 +0530

    added the field level validation handler

--- AmazonLite-Backend/AmazonLite-Backend/src/main/java/com/pranav/exception/GlobalExceptionHandler.java
+++ AmazonLite-Backend/AmazonLite-Backend/src/main/java/com/pranav/exception/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package com.pranav.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Date;
+import java.util.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,6 +20,21 @@ public class GlobalExceptionHandler {
     {
         ExceptionResponse response = ExceptionResponse.builder().message(request.getMessage()).status(HttpStatus.NOT_FOUND).date(new Date()).build();
         return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+    }
+
+    // MethodArgumentException exception
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException e)
+    {
+        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+        Map<String, Object> errors = new HashMap<>();
+        allErrors.forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 
